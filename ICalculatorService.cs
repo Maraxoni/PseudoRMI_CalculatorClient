855bc33333d8534c83e502e003e38efa209ac5d5
Maraxoni <103191241+Maraxoni@users.noreply.github.com>
2025-03-09 18:50:48 +0100

    Add project files.

---
+++ ICalculatorService.cs
@@ -0,0 +1,23 @@
+﻿using System.ServiceModel;
+
+namespace PseudoRMI_CalculatorClient
+{
+    [ServiceContract]
+    public interface ICalculatorService
+    {
+        [OperationContract]
+        double Calculate(string input);
+
+        [OperationContract]
+        double Add(double a, double b);
+
+        [OperationContract]
+        double Subtract(double a, double b);
+
+        [OperationContract]
+        double Multiply(double a, double b);
+
+        [OperationContract]
+        double Divide(double a, double b);
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.ServiceModel;
+using System.ServiceModel.Channels;
+
+namespace PseudoRMI_CalculatorClient 
+{
+    class CalculatorClient
+    {
+        static void Main(string[] args)
+        {
+            // Communication method
+            BasicHttpBinding httpBinding = new BasicHttpBinding();
+            // Defining address
+            System.ServiceModel.EndpointAddress httpEndpoint = new System.ServiceModel.EndpointAddress("http://localhost:5000/CalculatorService");
+            // Dynamically creating channels that implement interface
+            ChannelFactory<ICalculatorService> myChannelFactory = new ChannelFactory<ICalculatorService>(httpBinding, httpEndpoint);
+            
+            ICalculatorService wcfClient1 = myChannelFactory.CreateChannel();
+
+            while (true)
+            {
+                Console.WriteLine("Enter operation or 'exit'");
+                string input = Console.ReadLine();
+                if (input.ToLower() == "exit")
+                    break;
+
+                try
+                {
+                    // Wywołanie metody Calculate na usłudze
+                    double result = wcfClient1.Calculate(input);
+                    Console.WriteLine($"Result: {result}");
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine("Error: " + ex.Message);
+                }
+            }
+
+            myChannelFactory.Close();
+        }
+    }
+}
